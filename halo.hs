a573f36bb25e61f2fbd90221eef7ab69d0839a90
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-02-25 13:26:14 +0700

    tes lagi

--- halo.hs
+++ halo.hs
@@ -2,8 +2,4 @@ module Halo where
     halo :: IO()
     halo = putStrLn "Hallo Dunia !"
 
-    add :: Int -> Int -> Int
-    add a b = a + b
-
-    multiply :: Int -> Int -> Int
-    multiply a b = a * b
+    
