d649bad35ddb906688b8bc982052a6fb01e6d976
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-02-25 13:30:16 +0700

    add fib on halo.hs

--- halo.hs
+++ halo.hs
@@ -2,4 +2,9 @@ module Halo where
     halo :: IO()
     halo = putStrLn "Hallo Dunia !"
 
-    
+    fib :: Int -> Int
+    fib n
+        | n == 0 = 0
+        | n == 1 = 1
+        | n == 2 = 1
+        | otherwise = fib(n-2) + fib(n-1)
